bbe25dc7bfe93e6212198d42488b8a3a82508cc0
codie-yash <84841903+codie-yash@users.noreply.github.com>
2022-01-16 21:00:25 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -43,7 +43,7 @@ $(function ($) {
 
         // Typed js
         $(".typed").typed({
-            strings: ["Programmer.", "Security Researcher.", "Freelancer.", "Student."],
+            strings: ["Security Researcher.", "Programmer.", "PullStack-Dev.", "Student."],
             // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
             stringsElement: null,
             // typing speed
